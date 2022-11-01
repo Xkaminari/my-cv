5d1af5449c8761e81044cc46485b286cdd7769ed
Xkaminari <chahbounmedyassine@gmail.com>
2022-11-01 01:40:56 +0100

    HomeAnimations

--- src/LeftSide/Main/HomeContent/CtaAndTeaser/CtaAndTeaser.jsx
+++ src/LeftSide/Main/HomeContent/CtaAndTeaser/CtaAndTeaser.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import TeaserImg from "../Med-Yassine's-Profil-pick.webp"
 
+
 class CtaAndTeaser extends React.Component {
+    enterTeaser(){
+        let teaserText = document.getElementsByClassName('Teaser-text')[0];
+        teaserText.classList.add('Teaser-text-hover-form');
+        let btn1 = document.getElementsByClassName('module-border-wrap')[0];
+        btn1.classList.add('Btn-type1-hover-animation');
+    }
+
+    leaveTeaser(){
+        let teaserText = document.getElementsByClassName('Teaser-text')[0];
+        teaserText.classList.remove('Teaser-text-hover-form');
+        let btn1 = document.getElementsByClassName('module-border-wrap')[0];
+        btn1.classList.remove('Btn-type1-hover-animation');
+    }
+
     render() {
         return <div className="CtaAndTeaser">
-        <div className="Cta">
-            <button className="Btn-type1">Book an appointement</button>
-            <div className="Check-work">
-                <p className="Check-work-text">Check out my work</p>
-                <div className="Arrow">
-                    <div className="arrow-head"></div>
+            <div className="Teaser">
+                <div className="img-teaser-container" onMouseEnter={this.enterTeaser.bind(this)} onMouseLeave={this.leaveTeaser.bind(this)}>
+                <img className="img-teaser" src={TeaserImg} alt="Med Yassine chahboun profile pick" />
+                </div>
+                <div className="Teaser-details">
+                    <div className="Teaser-text">
+                        <p>Need a skilled developper? I'm your man.</p>
+                        <p>In the section below you will learn more </p>
+                        <p>about your future developper.</p>
+                    </div>
+                    <div className="module-border-wrap">
+                        <button className="Btn-type1">Explore</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="Teaser">
-            <div className="img-teaser-container">
-            <img className="img-teaser" src={TeaserImg} alt="Med Yassine chahboun profile pick" />
-            </div>
-            <div className="Teaser-details">
-                <div className="Teaser-text">
-                    <p>Need a work to be done? Iam your man.</p>
-                    <p>In the section below you will learn more </p>
-                    <p>about your future developper.</p>
-                </div>
-                <div className="module-border-wrap">
-                    <button className="Btn-type2">Explore</button>
-                </div>
-            </div>
-        </div>
         </div>
     }
 }
